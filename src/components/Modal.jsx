0b5b6a970504228b56c99386e98c478deb5e5832
Juanjo Ruiz <juanjoserusa@gmail.com>
2023-02-13 21:20:08 +0100

    primer commit falta responsive

---
+++ src/components/Modal.jsx
@@ -0,0 +1,113 @@
+import React, {useState, useEffect } from 'react'
+import Mensaje from './Mensaje'
+import CerrarBtn from '../img/cerrar.svg'
+
+const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar, setGastoEditar}) => {
+
+    const [nombre, setNombre] = useState('')
+    const [cantidad, setCantidad] = useState('')
+    const [categoria, setCategoria] = useState('')
+    const [fecha, setFecha] = useState('')
+    const [mensaje, setMensaje] =useState('')
+    const [id, setId] = useState('')
+
+    useEffect(() => {
+        if( Object.keys(gastoEditar).length > 0){
+            setNombre(gastoEditar.nombre)
+            setCantidad(gastoEditar.cantidad)
+            setCategoria(gastoEditar.categoria)
+            setId(gastoEditar.id)
+            setFecha(gastoEditar.fecha)
+          }
+    }, []);
+
+    const ocultarModal = () => {       
+        setAnimarModal(false)
+        setGastoEditar({})
+        setTimeout(() => {
+            setModal(false)
+        }, 500);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if([ nombre, cantidad, categoria].includes('')){
+            setMensaje('Todos los campos son obligatorios')
+
+            setTimeout(() => {
+                setMensaje('')
+            }, 3000);
+            return;
+        }
+
+        guardarGasto({nombre, cantidad, categoria, id, fecha})
+    }
+
+  return (
+    <div className='modal'>
+        <div className='cerrar-modal'>
+            <img 
+                src={CerrarBtn}
+                alt='Cerrar modal'
+                onClick={ocultarModal}
+            />
+        </div>
+        <form            
+            className={`formulario ${animarModal ? "animar" : 'cerrar'}`}
+            onSubmit={handleSubmit}
+        >
+            <legend>{gastoEditar.nombre ? ' Editar Gasto' : 'Nuevo gasto'}</legend>
+            {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
+            <div className='campo'>
+                <label htmlFor='nombre'>Nombre Gasto</label>
+                <input 
+                    id='nombre'
+                    type='text'
+                    placeholder='Añade el nombre del gasto'
+                    value={nombre}
+                    onChange={e => setNombre(e.target.value) }
+                    
+                    />
+            </div>
+            <div className='campo'>
+                <label htmlFor='nombre'>Cantidad</label>
+                <input 
+                    id='cantidad'
+                    type='number'
+                    placeholder='Añade la cantidad del gasto: ej: 200'
+                    value={cantidad}
+                    onChange={e => setCantidad(Number(e.target.value)) } 
+                    
+                    />
+            </div>
+
+            <div className='campo'>
+                <label htmlFor='nombre'>Categoria</label>
+                <select
+                    id='categoria'
+                    value={categoria}
+                    onChange={e => setCategoria(e.target.value) }
+                    >
+                    <option value=''>-- Seleccione --</option>
+                    <option value='ahorro'>Ahorro</option>
+                    <option value='comida'>Comida</option>
+                    <option value='casa'>Casa</option>
+                    <option value='gastos'>Gastos Varios</option>
+                    <option value='ocio'>Ocio</option>
+                    <option value='salud'>Salud</option>
+                    <option value='suscripciones'>Suscripciones</option>
+
+                </select>
+            </div>
+            <input 
+                type='submit'
+                value={gastoEditar.nombre ? 'Guardar Cambios' :'Añadir Gasto'}
+            />
+        </form>
+
+    </div>
+  )
+}
+
+export default Modal
